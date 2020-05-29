d5cfec407719d14e06c4908279e857d3d76f6aa4
patrick brisbin <pbrisbin@gmail.com>
2020-05-29 13:12:09 -0400

    Initial commit

---
+++ library/Test/Hspec/Expectations/Json.hs
@@ -0,0 +1,165 @@
+-- | Expectations on JSON 'Value's
+--
+-- Semantics:
+--
+-- +--------------------------+-------------------+-------------------+
+-- | Assertion that fails on: | extra Object keys | wrong Array order +
+-- +==========================+===================+===================+
+-- | 'shouldBeJson'           | Yes               | Yes               |
+-- +--------------------------+-------------------+-------------------+
+-- | 'shouldBeUnorderedJson'  | Yes               | No                |
+-- +--------------------------+-------------------+-------------------+
+-- | 'shouldMatchJson'        | No                | No                |
+-- +--------------------------+-------------------+-------------------+
+-- | 'shouldMatchOrderedJson' | No                | Yes               |
+-- +--------------------------+-------------------+-------------------+
+--
+module Test.Hspec.Expectations.Json
+  ( shouldBeJson
+  , shouldBeUnorderedJson
+  , shouldMatchJson
+  , shouldMatchOrderedJson
+
+  -- * As predicates
+  -- | These are only created when a specific need arises
+  , matchesJson
+  )
+where
+
+import Prelude
+
+import Data.Aeson
+import Data.Aeson.Encode.Pretty (encodePretty)
+import Data.Text.Lazy (toStrict)
+import Data.Text.Lazy.Encoding (decodeUtf8)
+import GHC.Stack
+import Test.Hspec.Expectations.Json.Internal
+
+-- $setup
+-- >>> :set -XQuasiQuotes
+-- >>> import Data.Aeson.QQ (aesonQQ)
+-- >>> import Test.HUnit.Lang (HUnitFailure(..), formatFailureReason)
+-- >>> import Control.Exception (handle)
+-- >>> let printFailure (HUnitFailure _ r) = putStr $ formatFailureReason r
+-- >>> let catchFailure f = handle printFailure $ f >> putStrLn "<passed>"
+
+-- | Compare two JSON values, with a useful diff
+--
+-- >>> :{
+-- catchFailure $
+--   [aesonQQ| { "a": true, "b": false } |] `shouldBeJson`
+--   [aesonQQ| { "a": true, "b": false } |]
+-- :}
+-- <passed>
+--
+-- >>> :{
+-- catchFailure $
+--   [aesonQQ| { "a": true, "b": false } |] `shouldBeJson`
+--   [aesonQQ| { "a": true, "b": true  } |]
+-- :}
+--    {
+--        "a": true,
+-- ---    "b": true
+-- +++    "b": false
+--    }
+--
+shouldBeJson :: HasCallStack => Value -> Value -> IO ()
+shouldBeJson a b = assertBoolWithDiff (a == b) (toText b) (toText a)
+  where toText = toStrict . decodeUtf8 . encodePretty . normalizeScientific
+
+infix 1 `shouldBeJson`
+
+-- | 'shouldBeJson', ignoring Array ordering
+--
+-- >>> :{
+-- catchFailure $
+--   [aesonQQ| { "a": [true, false], "b": false } |] `shouldBeUnorderedJson`
+--   [aesonQQ| { "a": [false, true], "b": false } |]
+-- :}
+-- <passed>
+--
+-- >>> :{
+-- catchFailure $
+--   [aesonQQ| { "a": [true, false], "b": false, "c": true } |] `shouldBeUnorderedJson`
+--   [aesonQQ| { "a": [false, true], "b": true             } |]
+-- :}
+--    {
+--        "a": [
+--            false,
+--            true
+--        ],
+-- ---    "b": true
+-- +++    "b": false,
+-- +++    "c": true
+--    }
+--
+shouldBeUnorderedJson :: HasCallStack => Value -> Value -> IO ()
+shouldBeUnorderedJson a b = sortJsonArrays a `shouldBeJson` sortJsonArrays b
+
+infix 1 `shouldBeUnorderedJson`
+
+-- | 'shouldBeJson', ignoring extra Object keys or Array ordering
+--
+-- >>> :{
+-- catchFailure $
+--   [aesonQQ| { "a": [true, false], "b": false, "c": true } |] `shouldMatchJson`
+--   [aesonQQ| { "a": [false, true], "b": false            } |]
+-- :}
+-- <passed>
+--
+-- >>> :{
+-- catchFailure $
+--   [aesonQQ| { "a": [true, false], "b": false, "c": true } |] `shouldMatchJson`
+--   [aesonQQ| { "a": [false, true], "b": true             } |]
+-- :}
+--    {
+--        "a": [
+--            false,
+--            true
+--        ],
+-- ---    "b": true
+-- +++    "b": false
+--    }
+--
+shouldMatchJson :: HasCallStack => Value -> Value -> IO ()
+shouldMatchJson sup sub =
+  sortJsonArrays (pruneJson (Superset sup) (Subset sub))
+    `shouldBeJson` sortJsonArrays sub
+
+infix 1 `shouldMatchJson`
+
+-- | Compare JSON values with the same semantics as 'shouldMatchJson'
+matchesJson :: Value -> Value -> Bool
+matchesJson sup sub =
+  sortJsonArrays (pruneJson (Superset sup) (Subset sub)) == sortJsonArrays sub
+
+-- | 'shouldBeJson', ignoring extra Object keys
+--
+-- >>> :{
+-- catchFailure $
+--   [aesonQQ| { "a": [true, false], "b": false, "c": true } |] `shouldMatchOrderedJson`
+--   [aesonQQ| { "a": [true, false], "b": false            } |]
+-- :}
+-- <passed>
+--
+-- >>> :{
+-- catchFailure $
+--   [aesonQQ| { "a": [true, false], "b": false, "c": true } |] `shouldMatchOrderedJson`
+--   [aesonQQ| { "a": [false, true], "b": true             } |]
+-- :}
+--    {
+--        "a": [
+-- ---        false,
+-- ---        true
+-- +++        true,
+-- +++        false
+--        ],
+-- ---    "b": true
+-- +++    "b": false
+--    }
+--
+shouldMatchOrderedJson :: HasCallStack => Value -> Value -> IO ()
+shouldMatchOrderedJson sup sub =
+  pruneJson (Superset sup) (Subset sub) `shouldBeJson` sub
+
+infix 1 `shouldMatchOrderedJson`
---
+++ tests/Test/Hspec/Expectations/Json/InternalSpec.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Test.Hspec.Expectations.Json.InternalSpec
+  ( spec
+  )
+where
+
+import Prelude
+
+import Data.Aeson.QQ
+import Test.Hspec
+import Test.Hspec.Expectations.Json (shouldBeJson)
+import Test.Hspec.Expectations.Json.Internal
+
+spec :: Spec
+spec = do
+  describe "pruneJson" $ do
+    it "prunes object keys from Superset not present in Subset" $ do
+      let
+        sup = Superset [aesonQQ|{ "foo": "foo", "baz": "baz" }|]
+        sub = Subset [aesonQQ|{ "foo": "bar" } |]
+
+      pruneJson sup sub `shouldBeJson` [aesonQQ|{ "foo": "foo" }|]
+
+    it "prunes object keys recursively Superset not present in Subset" $ do
+      let
+        sup = Superset [aesonQQ|
+          { "foo": { "bar": "bar" , "baz": "baz" }
+          , "bat": "bat"
+          }
+        |]
+
+        sub = Subset [aesonQQ|
+          { "foo": { "bar": "baz" }
+          }
+        |]
+
+      pruneJson sup sub `shouldBeJson` [aesonQQ|{ "foo": { "bar": "bar" } }|]
+
+    it "prunes objects within Arrays" $ do
+      let
+        sup = Superset [aesonQQ|
+          [ { "foo": "bar", "quix": "cats" }
+          , { "foo": "bats", "quix": "baz" }
+          ]
+        |]
+
+        sub = Subset [aesonQQ|
+          [ { "foo": "zap" }
+          , { "foo": "zop" }
+          ]
+        |]
+
+      pruneJson sup sub `shouldBeJson` [aesonQQ|
+       [ { "foo": "bar" }
+       , { "foo": "bats" }
+       ]
+      |]
+
+    it "handles mismatching types" $ do
+      let
+        sup = Superset [aesonQQ|
+          { "foo": { "bar": 1, "baz": "baz" }
+          , "bat": { "bat": 1 }
+          }
+        |]
+
+        sub = Subset [aesonQQ|
+          { "foo": { "bar": "baz" }
+          , "bat": "bat"
+          }
+        |]
+
+      pruneJson sup sub `shouldBeJson` [aesonQQ|
+        { "foo": { "bar": 1 }
+        , "bat": { "bat": 1 }
+        }
+      |]
+
+  describe "sortJsonArrays" $ do
+    it "sorts arrays" $ do
+      let
+        unsorted = [aesonQQ|["number_facts", "number_basics"]|]
+        sorted = [aesonQQ|["number_basics", "number_facts"]|]
+
+      sortJsonArrays unsorted `shouldBeJson` sorted
+
+    it "sorts arrays in object keys" $ do
+      let
+        unsorted = [aesonQQ|{ "x": ["number_facts", "number_basics"] }|]
+        sorted = [aesonQQ|{ "x": ["number_basics", "number_facts"] }|]
+
+      sortJsonArrays unsorted `shouldBeJson` sorted
+
+    it "works on arrays of nested arrays" $ do
+      let
+        unsorted = [aesonQQ|[{ "x": ["number_facts", "number_basics"] }]|]
+        sorted = [aesonQQ|[{ "x": ["number_basics", "number_facts"] }]|]
+
+      sortJsonArrays unsorted `shouldBeJson` sorted
